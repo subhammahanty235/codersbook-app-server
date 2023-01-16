8f704aa5cb909958274ca8d099ffa3bdfe930018
subhammahanty235 <subhammahanty235@gmail.com>
2023-01-16 20:14:10 +0530

    Project added to repo

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -5,9 +6,20 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json());
-app.use(cors());
-mongoose.connect("mongodb+srv://subham235:subham1234@cluster0.wuy84.mongodb.net/socialmediav2?retryWrites=true&w=majority").then(console.log("Database is connected")).catch((err) => { console.log(err) })
-
+// app.use(cors());
+mongoose.connect(process.env.MONGODB_URI).then(console.log("Database is connected")).catch((err) => { console.log(err) })
+const whitelist = ["https://codersbook.netlify.app", "http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 app.use('/api/auth', require('./routes/userroute'))
 app.use('/api/post', require('./routes/postroute'))
 app.listen(5000, () => {
